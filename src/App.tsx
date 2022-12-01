83f1a9b8db5877b23279677a3518dbb85f10f6fc
Mathias <mathiasnouzy@gmail.com>
2022-12-01 18:31:06 +0100

    chore: husky 3

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button className="text-orange-500 mb-5" onClick={() => setCount((count) => count + 1)}>
+        <button className="text-red-500 mb-5" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p className="text-blue-500">
