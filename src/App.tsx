c9a9eb0f6094d08938dbbb5020fccb738ceb39ca
Mathias <mathiasnouzy@gmail.com>
2022-12-06 11:53:05 +0100

    style: add tailwind colors

--- src/App.tsx
+++ src/App.tsx
@@ -16,16 +16,19 @@ function App() {
           <img alt="React logo" className="logo react" src={reactLogo} />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button className="text-orange-500 mb-5" onClick={() => setCount((count) => count + 1)}>
+      <h1>
+        <span className="text-turquoise-200">Vite</span> +{" "}
+        <span className="text-turquoise-500">React</span>
+      </h1>
+      <div className="p-2">
+        <button className="text-blue-500 mb-5" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p className="text-blue-500">
+        <p className="text-turquoise-500">
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <p className="text-gray-500">Click on the Vite and React logos to learn more</p>
     </div>
   );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          500: "#505050",
+        },  
+        blue: {
+          500: "#182D70"
+        },
+        turquoise: {
+          
+          200: "#86CEE4",
+          500: "#01A6D8"
+        }
+      }
+    },
   },
   plugins: [],
 }
